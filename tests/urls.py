e4f72cbf3c493048de90dea255a3037a7b849421
Norman Jäckel <ophrys@normanjaeckel.de>
2013-08-14 02:40:53 +0200

    Add AutoModel class. Enhance GetAbsoluteUrlMixin.

--- tests/urls.py
+++ tests/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, include, url
 
+from tests.models import TestModelC
+
 
 test_model_a_urlpatterns = patterns(
     '',
+    url(r'^ohl9pheengooLai0noo1/', 'some_view', name='list'),
     url(r'^ahquee1daiQuo7wei0le/(?P<pk>\d+)/', 'some_view', name='detail'),
     url(r'^lahQuo2pheeM0iech7ku/(?P<pk>\d+)/', 'some_view', name='update'))
 
@@ -16,7 +19,8 @@ test_model_b_urlpatterns = patterns(
 test_model_urlpatterns = patterns(
     '',
     url(r'^test_model_a/', include(test_model_a_urlpatterns, namespace='TestModelA')),
-    url(r'^test_model_b/', include(test_model_b_urlpatterns, namespace='TestModelB')))
+    url(r'^test_model_b/', include(test_model_b_urlpatterns, namespace='TestModelB')),
+    url(r'^test_model_c/', include(TestModelC().urls)))
 
 
 urlpatterns = patterns(
---
+++ tests/utils/test_views.py
@@ -0,0 +1,41 @@
+from django.template import TemplateDoesNotExist
+from django.test import TestCase
+from django.test.client import Client
+
+from tests.models import TestModelC
+
+
+class AutoModelViewsTest(TestCase):
+    urls = 'tests.urls'
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_list_view(self):
+        self.assertRaisesMessage(TemplateDoesNotExist, 'tests/testmodelc_list.html', self.client.get, '/test_model/test_model_c/')
+
+    def test_create_view(self):
+        self.assertRaisesMessage(TemplateDoesNotExist, 'tests/testmodelc_form.html', self.client.get, '/test_model/test_model_c/create/')
+        response = self.client.post('/test_model/test_model_c/create/', {'name': 'aar7iNgou2viw4ahv2ve'})
+        self.assertEqual(response.url, 'http://testserver/test_model/test_model_c/1/')
+        self.assertTrue(TestModelC.objects.filter(name='aar7iNgou2viw4ahv2ve').exists())
+
+    def test_detail_view(self):
+        TestModelC.objects.create(name='AiwaiB5oheez1UJajeip')
+        self.assertRaisesMessage(TemplateDoesNotExist, 'tests/testmodelc_detail.html', self.client.get, '/test_model/test_model_c/1/')
+
+    def test_update_view(self):
+        TestModelC.objects.create(name='Luph3tohquoesai2haLa')
+        self.assertRaisesMessage(TemplateDoesNotExist, 'tests/testmodelc_form.html', self.client.get, '/test_model/test_model_c/1/update/')
+        response = self.client.post('/test_model/test_model_c/1/update/', {'name': 'apeej7eiGhab3eipheem'})
+        self.assertEqual(response.url, 'http://testserver/test_model/test_model_c/1/')
+        self.assertFalse(TestModelC.objects.filter(name='Luph3tohquoesai2haLa').exists())
+        self.assertTrue(TestModelC.objects.filter(name='apeej7eiGhab3eipheem').exists())
+
+    def test_delete_view(self):
+        TestModelC.objects.create(name='Zah6ceiGiu0ahf7ouH4h')
+        self.assertTrue(TestModelC.objects.filter(name='Zah6ceiGiu0ahf7ouH4h').exists())
+        self.assertRaisesMessage(TemplateDoesNotExist, 'tests/testmodelc_confirm_delete.html', self.client.get, '/test_model/test_model_c/1/delete/')
+        response = self.client.post('/test_model/test_model_c/1/delete/', {})
+        self.assertEqual(response.url, 'http://testserver/test_model/test_model_c/')
+        self.assertFalse(TestModelC.objects.filter(name='Zah6ceiGiu0ahf7ouH4h').exists())
